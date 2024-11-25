41ff21260cc3ed95b5800fb5dcf2a87c8d0ffe6c
EnzoFenato <enzofenato@edu.unifil.br>
2024-11-25 19:42:39 -0300

    soma-funcionalidade
    aqui esta uma soma dentro do codigo

--- vers.py
+++ vers.py
@@ -1,4 +1,8 @@
-print("Hello world")
-def exibir_mensagem():
-    return "Esta é uma nova funcionalidade!"
-print(exibir_mensagem())
+def soma(a, b):
+    return a + b
+
+valor1 = float(input("Insira o primeiro número: "))
+valor2 = float(input("Insira o segundo número: "))
+
+resultado = soma(valor1, valor2)
+print(f"A soma de {valor1} e {valor2} é {resultado}")
